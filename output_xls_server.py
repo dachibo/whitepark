c6b70b417961f439d27de725695a74a90294b945
dachibo <qoop00@mail.ru>
2021-06-12 22:23:40 +0500

    fix

--- output_xls_server.py
+++ output_xls_server.py
@@ -4,9 +4,9 @@ import requests
 from config import ip
 
 data = str(datetime.now().date())
-name_xls = f'{data}.xls'
+
 if os.path.exists(name_xls):
-    with open(f'/var/www/u1250062/data/{name_xls}', 'rb') as file_bytes:
+    with open(f'/var/www/u1250062/data/{data}.xls', 'rb') as file_bytes:
         headers = {
             'Content-Type': 'text/plain',
         }
